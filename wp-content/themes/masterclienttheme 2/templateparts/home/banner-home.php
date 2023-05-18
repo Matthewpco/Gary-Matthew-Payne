fc8331af5339a28bd82d25b23d8f1b8bc75dd073
matthewpco <matthewpco86@gmail.com>
2023-05-18 19:24:40 +0000

    [+] updated home page

--- wp-content/themes/masterclienttheme 2/templateparts/home/banner-home.php
+++ wp-content/themes/masterclienttheme 2/templateparts/home/banner-home.php
@@ -1,9 +1,8 @@
-
 <!-- MAIN BANNER START -->
 <div class="sec-john-1">
 	<div class="container">
 		<div class="john-blk-main" data-aos="fade-up" data-aos-duration="1000">
-			<h5>Hello!</h5>
+			<h5>Hello!!</h5>
 			<h1><?php echo get_option("client_name"); ?></h1>
 			<h6><?php echo get_option("client_desc"); ?></h6>
 		</div>
